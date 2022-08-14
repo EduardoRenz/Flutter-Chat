615af5d0fe1eff46763c08cefa1589a658b827e4
Eduardo <dudarenz@gmail.com>
2022-08-13 23:06:50 -0300

    implemented auth mock service

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,33 +1,49 @@
-import 'package:chat/core/models/chat_user.dart';
+import 'dart:async';
 import 'dart:io';
-
+import 'dart:math';
+import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    setCurrentUser(null);
+  });
+
   @override
-  // TODO: implement currentUser
-  ChatUser? get currentUser => throw UnimplementedError();
+  ChatUser? get currentUser => _currentUser;
 
   @override
   Future<void> login(String email, String password) async {
-    // TODO: implement login
-    throw UnimplementedError();
+    setCurrentUser(_users[email]);
   }
 
   @override
   Future<void> logout() async {
-    // TODO: implement logout
-    throw UnimplementedError();
+    setCurrentUser(null);
   }
 
   @override
   Future<void> signUp(
       String name, String email, String password, File image) async {
-    // TODO: implement signUp
-    throw UnimplementedError();
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageUrl: image.path,
+    );
+    _users.putIfAbsent(email, () => newUser);
+    setCurrentUser(newUser);
   }
 
   @override
-  // TODO: implement userChanges
-  Stream<ChatUser> get userChanges => throw UnimplementedError();
+  Stream<ChatUser?> get userChanges => _userStream;
+
+  static void setCurrentUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
 }
