47dffc2f86fb654ab166550cf74d73b20026cb07
Eduardo <dudarenz@gmail.com>
2022-08-16 00:14:06 -0300

    added avatars in chat bubble

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,45 +1,93 @@
+import 'dart:io';
+
 import 'package:chat/core/models/chat_message.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
+  static const _defaultImage = 'assets/images/avatar.png';
   final ChatMessage message;
   final bool belongsToCurrentUser;
   const MessageBubble(
       {Key? key, required this.message, required this.belongsToCurrentUser})
       : super(key: key);
 
+  Widget _showUserImage(String imageURL) {
+    ImageProvider? provider;
+    final uri = Uri.parse(imageURL);
+    if (uri.path.contains(_defaultImage)) {
+      provider = AssetImage(imageURL);
+    } else if (uri.scheme == 'http' || uri.scheme == 'https') {
+      provider = NetworkImage(imageURL);
+    } else {
+      provider = FileImage(File(imageURL));
+    }
+
+    return CircleAvatar(
+      backgroundImage: provider,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: belongsToCurrentUser
-          ? MainAxisAlignment.start
-          : MainAxisAlignment.end,
+    const borderRadius = Radius.circular(12);
+    final bubbleSize = MediaQuery.of(context).size.width * 0.8;
+    return Stack(
+      clipBehavior: Clip.none,
       children: [
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          padding: const EdgeInsets.all(8),
-          width: MediaQuery.of(context).size.width * 0.8,
-          decoration: BoxDecoration(
-              color: belongsToCurrentUser
-                  ? Colors.grey[300]
-                  : Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.circular(12)),
-          child: Column(
-            children: [
-              Text(
-                message.userName,
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      color: belongsToCurrentUser ? Colors.black : Colors.white,
-                    ),
+        Row(
+          mainAxisAlignment: belongsToCurrentUser
+              ? MainAxisAlignment.start
+              : MainAxisAlignment.end,
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 8),
+              padding: const EdgeInsets.all(10),
+              width: bubbleSize,
+              decoration: BoxDecoration(
+                  color: belongsToCurrentUser
+                      ? Colors.grey[300]
+                      : Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: borderRadius,
+                    topRight: borderRadius,
+                    bottomLeft: belongsToCurrentUser
+                        ? const Radius.circular(0)
+                        : borderRadius,
+                    bottomRight: !belongsToCurrentUser
+                        ? const Radius.circular(0)
+                        : borderRadius,
+                  )),
+              child: Column(
+                crossAxisAlignment: belongsToCurrentUser
+                    ? CrossAxisAlignment.start
+                    : CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    message.userName,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: belongsToCurrentUser
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                  ),
+                  Text(
+                    message.text,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: belongsToCurrentUser
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                  ),
+                ],
               ),
-              Text(
-                message.text,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: belongsToCurrentUser ? Colors.black : Colors.white,
-                    ),
-              ),
-            ],
-          ),
+            )
+          ],
+        ),
+        Positioned(
+          top: -5,
+          left: belongsToCurrentUser ? bubbleSize - 20 : 60,
+          child: _showUserImage(message.userImageURL),
         )
       ],
     );
