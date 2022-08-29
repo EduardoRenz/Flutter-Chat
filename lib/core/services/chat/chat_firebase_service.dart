46e9c992d3ecca73efc5819191f94c29b2984025
Eduardo <dudarenz@gmail.com>
2022-08-29 00:35:59 -0300

    stream of messages

--- lib/core/services/chat/chat_firebase_service.dart
+++ lib/core/services/chat/chat_firebase_service.dart
@@ -6,7 +6,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ChatFirebaseService implements ChatService {
   @override
   Stream<List<ChatMessage>> messageStream() {
-    return const Stream<List<ChatMessage>>.empty();
+    final store = FirebaseFirestore.instance;
+    final snapshots = store
+        .collection('chat')
+        .withConverter(fromFirestore: _fromFirestore, toFirestore: _toFirestore)
+        .snapshots();
+
+    return snapshots.map(
+      (snapshot) => snapshot.docs.map((doc) => doc.data()).toList(),
+    );
+
+    // return Stream<List<ChatMessage>>.multi((controller) {
+    //   snapshots.listen((snapshot) {
+    //     List<ChatMessage> list =
+    //         snapshot.docs.map((doc) => doc.data()).toList();
+    //     controller.add(list);
+    //   });
+    // });
   }
 
   @override
